0b2729e540727c7871aeb7de24db780c2ef2438a
Sebastien Coutu <scoutu@mnubo.com>
2016-01-22 20:41:46 -0500

    Fixing packaging

--- setup.py
+++ setup.py
@@ -7,13 +7,6 @@ except ImportError:
 # See https://github.com/pybuilder/pybuilder/issues/56
 del os.link
 
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
-
 def version():
     with open(os.path.abspath(__file__).replace('setup.py', 'version.meta'), 'r') as v:
         return v.read()
@@ -21,11 +14,11 @@ def version():
 setup(name='kubernetes-py',
       version=version(),
       description='A python module for Kubernetes.',
-      long_description=read_md('README.md'),
       author='mnubo inc.',
       author_email='scoutu@mnubo.com',
       url='https://github.com/mnubo/kubernetes-py',
-      download_url='https://github.com/mnubo/kubernetes-py/releases',
+      download_url='https://github.com/mnubo/kubernetes-py/tarball/' + version(),
+      keywords=['kubernetes', 'k8s'],
       packages=[
           'kubernetes',
           'kubernetes.exceptions',
