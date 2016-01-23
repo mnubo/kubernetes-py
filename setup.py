3f240efb5e511ebdb8e2692521f177fdc0509a6d
Sebastien Coutu <scoutu@mnubo.com>
2016-01-22 20:29:51 -0500

    Adding base files so package can build properly.

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ except ImportError:
 # See https://github.com/pybuilder/pybuilder/issues/56
 del os.link
 
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+
 def version():
     with open(os.path.abspath(__file__).replace('setup.py', 'version.meta'), 'r') as v:
         return v.read()
@@ -14,12 +21,17 @@ def version():
 setup(name='kubernetes-py',
       version=version(),
       description='A python module for Kubernetes.',
+      long_description=read_md('README.md'),
       author='mnubo inc.',
       author_email='scoutu@mnubo.com',
       url='https://github.com/mnubo/kubernetes-py',
       download_url='https://github.com/mnubo/kubernetes-py/releases',
       packages=[
-          'kubernetes'
+          'kubernetes',
+          'kubernetes.exceptions',
+          'kubernetes.models',
+          'kubernetes.models.v1',
+          'kubernetes.utils'
       ],
       install_requires=[
           'importlib>=1.0.3',
