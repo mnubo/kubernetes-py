83f013ea96c31de23c394c85ff9d038e018bb8bb
Samuel BERTHE <contact@samuel-berthe.fr>
2016-05-15 14:24:56 +0200

    feat(http,config): Added authentication by token and http basic auth

--- kubernetes/K8sConfig.py
+++ kubernetes/K8sConfig.py
@@ -1,5 +1,5 @@
 class K8sConfig:
-    def __init__(self, api_host='localhost:8888', version='v1', namespace='default', pull_secret=None):
+    def __init__(self, api_host='localhost:8888', version='v1', namespace='default', pull_secret=None, auth=None, token=None):
         valid_versions = ['v1']
         if not isinstance(api_host, str) or not isinstance(version, str):
             raise SyntaxError('Please make sure api_host and version are strings.')
@@ -7,10 +7,16 @@ class K8sConfig:
             raise SyntaxError('Please provide a valid version in: {str}'.format(str=', '.join(valid_versions)))
         if not isinstance(namespace, str):
             raise SyntaxError('Please make sure namespace is a string.')
+        if not isinstance(auth, tuple):
+            raise SyntaxError('Please make sure auth is a tuple.')
+        if not isinstance(namespace, str):
+            raise SyntaxError('Please make sure token is a string.')
         self.api_host = api_host
         self.version = version
         self.namespace = namespace
         self.pull_secret = pull_secret
+        self.auth = auth
+        self.token = token
 
     def get_api_host(self):
         return self.api_host
@@ -20,6 +26,12 @@ class K8sConfig:
 
     def get_pull_secret(self):
         return self.pull_secret
+
+    def get_auth(self):
+        return self.auth
+
+    def get_token(self):
+        return self.token
 
     def get_version(self):
         return self.version
@@ -35,6 +47,16 @@ class K8sConfig:
     def set_pull_secret(self, pull_secret):
         assert isinstance(pull_secret, str)
         self.pull_secret = pull_secret
+        return self
+
+    def set_auth(self, auth):
+        assert isinstance(auth, tuple)
+        self.auth = auth
+        return self
+
+    def set_token(self, token):
+        assert isinstance(token, str)
+        self.token = token
         return self
 
     def set_version(self, version=None):
