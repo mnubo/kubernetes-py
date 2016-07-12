2d1daa2751542b9ee31de091d224f3e73d19945b
Francis Roch <francis@manafont.net>
2016-07-12 11:00:14 -0400

    K8sConfig: - extract defaults to toplevel - add regex to check that api_host is a valid URL

--- kubernetes/K8sConfig.py
+++ kubernetes/K8sConfig.py
@@ -1,24 +1,39 @@
 import re
 
+DEFAULT_API_HOST = "localhost:8888"
+DEFAULT_API_VERSION = "v1"
+DEFAULT_NAMESPACE = "default"
+
+VALID_API_VERSIONS = ["v1"]
+
 
 class K8sConfig:
-    def __init__(self, api_host='localhost:8888', version='v1', namespace='default', pull_secret=None,
-                 auth=None, token=None):
-        valid_versions = ['v1']
+    def __init__(self, api_host=DEFAULT_API_HOST, version=DEFAULT_API_VERSION,
+                 namespace=DEFAULT_NAMESPACE, pull_secret=None, auth=None, token=None):
+
         if not isinstance(api_host, str) or not isinstance(version, str):
             raise SyntaxError('Please make sure api_host and version are strings.')
-        if version not in valid_versions:
-            raise SyntaxError('Please provide a valid version in: {str}'.format(str=', '.join(valid_versions)))
+
+        if version not in VALID_API_VERSIONS:
+            valid = ", ".join(VALID_API_VERSIONS)
+            raise SyntaxError('Please provide a valid version in: {0}'.format(valid))
+
         if not isinstance(namespace, str):
             raise SyntaxError('Please make sure namespace is a string.')
+
         if auth is not None and not isinstance(auth, tuple):
             raise SyntaxError('Please make sure auth is a tuple.')
+
         if token is not None and not isinstance(token, str):
             raise SyntaxError('Please make sure token is a string.')
 
         http_https_re = re.compile(r"^http[s]*")
         if not http_https_re.search(api_host):
             api_host = "http://{host}".format(host=api_host)
+
+        valid_url_re = re.compile("(https?\:\/\/)([a-z0-9-.]*)(\:)([0-9]{2,5})")
+        if not valid_url_re.match(api_host):
+            raise SyntaxError('Please make sure the API host is valid: {0}'.format(api_host))
 
         self.api_host = api_host
         self.version = version
