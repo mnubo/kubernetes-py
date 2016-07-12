6bc45dfa000c74ba9a8ad009e922988a86f48a42
Francis Roch <francis@manafont.net>
2016-07-12 10:57:52 -0400

    exceptions: add LICENSE.md header

--- kubernetes/exceptions/UnprocessableEntityException.py
+++ kubernetes/exceptions/UnprocessableEntityException.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
 
 class UnprocessableEntityException(Exception):
     def __init__(self, *args, **kwargs):
