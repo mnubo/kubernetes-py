1650c9245e1cd635c5a9242b5d7a66f4e4769190
Francis Roch <francis@manafont.net>
2016-07-14 15:24:25 -0400

    K8sContainer: add structure tests

--- tests/test_k8s_container.py
+++ tests/test_k8s_container.py
@@ -8,6 +8,7 @@
 
 import unittest
 from kubernetes import K8sContainer
+from kubernetes.models.v1 import Container
 
 
 class K8sContainerTest(unittest.TestCase):
@@ -52,3 +53,52 @@ class K8sContainerTest(unittest.TestCase):
         self.assertEqual(c.model.get_name(), name)
         self.assertEqual(c.model.get_image(), image)
 
+    # ------------------------------------------------------------------------------------- struct
+
+    def test_struct_k8scontainer(self):
+        name = "yomama"
+        image = "busybox"
+        c = K8sContainer(name=name, image=image)
+        self.assertIsNotNone(c)
+        self.assertIsInstance(c, K8sContainer)
+        self.assertIsNotNone(c.model)
+        self.assertIsInstance(c.model, Container)
+
+    def test_struct_container(self):
+        name = "yomama"
+        image = "busybox"
+        c = K8sContainer(name=name, image=image)
+        self.assertIsInstance(c.model, Container)
+        self.assertIsNone(c.model.liveness_probe)
+        self.assertIsNone(c.model.readiness_probe)
+        self.assertIsNotNone(c.model.model)
+
+    def test_struct_container_model(self):
+        name = "yomama"
+        image = "busybox"
+        c = K8sContainer(name=name, image=image)
+        model = c.model.model
+        self.assertIsInstance(model, dict)
+        self.assertEqual(7, len(model))
+        self.assertIn('hostNetwork', model)
+        self.assertIsInstance(model['hostNetwork'], bool)
+        self.assertIn('image', model)
+        self.assertIsInstance(model['image'], str)
+        self.assertIn('imagePullPolicy', model)
+        self.assertIsInstance(model['imagePullPolicy'], str)
+        self.assertIn('name', model)
+        self.assertIsInstance(model['name'], str)
+        self.assertEqual(model['name'], name)
+        self.assertIn('privileged', model)
+        self.assertIsInstance(model['privileged'], bool)
+        self.assertIn('resources', model)
+        self.assertIsInstance(model['resources'], dict)
+        self.assertEqual(1, len(model['resources']))
+        self.assertIn('requests', model['resources'])
+        self.assertIsInstance(model['resources']['requests'], dict)
+        self.assertEqual(2, len(model['resources']['requests']))
+        self.assertIn('cpu', model['resources']['requests'])
+        self.assertIsInstance(model['resources']['requests']['cpu'], str)
+        self.assertIn('memory', model['resources']['requests'])
+        self.assertIsInstance(model['resources']['requests']['memory'], str)
+        self.assertIn('terminationMessagePath', model)
