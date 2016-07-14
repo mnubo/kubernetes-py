b5bf564f275f8c50dd1bc5c9036da8f9f8321372
Francis Roch <francis@manafont.net>
2016-07-14 15:37:55 -0400

    K8sPod: rework structure tests

--- tests/test_k8s_pod.py
+++ tests/test_k8s_pod.py
@@ -8,6 +8,7 @@
 
 import unittest
 from kubernetes import K8sPod, K8sConfig
+from kubernetes.models.v1 import Pod, ObjectMeta, PodSpec
 
 
 class K8sPodTest(unittest.TestCase):
@@ -34,38 +35,6 @@ class K8sPodTest(unittest.TestCase):
         self.assertIsInstance(pod, K8sPod)
         self.assertEqual(pod.name, name)
 
-    def test_init_with_name_check_model_as_dict(self):
-        name = "yomama"
-        pod = K8sPod(name=name)
-        self.assertIsNotNone(pod)
-        dico = pod.as_dict()
-        self.assertIsNotNone(dico)
-        self.assertIn('apiVersion', dico)
-        self.assertIsInstance(dico['apiVersion'], str)
-        self.assertIn('kind', dico)
-        self.assertIsInstance(dico['kind'], str)
-        self.assertIn('metadata', dico)
-        self.assertIsInstance(dico['metadata'], dict)
-        self.assertIn('labels', dico['metadata'])
-        self.assertIsInstance(dico['metadata']['labels'], dict)
-        self.assertIn('name', dico['metadata']['labels'])
-        self.assertEqual(dico['metadata']['labels']['name'], name)
-        self.assertIn('name', dico['metadata'])
-        self.assertIsInstance(dico['metadata']['name'], str)
-        self.assertEqual(dico['metadata']['name'], name)
-        self.assertIn('namespace', dico['metadata'])
-        self.assertIsInstance(dico['metadata']['namespace'], str)
-        self.assertIn('spec', dico)
-        self.assertIsInstance(dico['spec'], dict)
-        self.assertIn('containers', dico['spec'])
-        self.assertIsInstance(dico['spec']['containers'], list)
-        self.assertIn('dnsPolicy', dico['spec'])
-        self.assertIsInstance(dico['spec']['dnsPolicy'], str)
-        self.assertIn('restartPolicy', dico['spec'])
-        self.assertIsInstance(dico['spec']['restartPolicy'], str)
-        self.assertIn('volumes', dico['spec'])
-        self.assertIsInstance(dico['spec']['volumes'], list)
-
     def test_init_with_config_and_pull_secrets(self):
         ps = "yomama"
         name = "sofat"
@@ -73,6 +42,58 @@ class K8sPodTest(unittest.TestCase):
         pod = K8sPod(config=config, name=name)
         self.assertIsNotNone(pod.config)
         self.assertEqual(ps, pod.config.pull_secret)
+
+    # ------------------------------------------------------------------------------------- struct
+
+    def test_struct_k8spod(self):
+        name = "yomama"
+        pod = K8sPod(name=name)
+        self.assertIsNotNone(pod)
+        self.assertIsInstance(pod, K8sPod)
+        self.assertIsNotNone(pod.model)
+        self.assertIsInstance(pod.model, Pod)
+
+    def test_struct_pod(self):
+        name = "yomama"
+        pod = K8sPod(name=name)
+        model = pod.model
+        self.assertIsInstance(model.model, dict)
+        self.assertIsInstance(model.pod_metadata, ObjectMeta)
+        self.assertIsInstance(model.pod_spec, PodSpec)
+        self.assertIsNone(model.pod_status)
+
+    def test_struct_pod_model(self):
+        name = "yomama"
+        pod = K8sPod(name=name)
+        model = pod.model
+        self.assertIsInstance(model, Pod)
+        self.assertIsNotNone(model)
+        self.assertIn('apiVersion', model)
+        self.assertIsInstance(model['apiVersion'], str)
+        self.assertIn('kind', model)
+        self.assertIsInstance(model['kind'], str)
+        self.assertIn('metadata', model)
+        self.assertIsInstance(model['metadata'], dict)
+        self.assertIn('labels', model['metadata'])
+        self.assertIsInstance(model['metadata']['labels'], dict)
+        self.assertIn('name', model['metadata']['labels'])
+        self.assertEqual(model['metadata']['labels']['name'], name)
+        self.assertIn('name', model['metadata'])
+        self.assertIsInstance(model['metadata']['name'], str)
+        self.assertEqual(model['metadata']['name'], name)
+        self.assertIn('namespace', model['metadata'])
+        self.assertIsInstance(model['metadata']['namespace'], str)
+        self.assertIn('spec', model)
+        self.assertIsInstance(model['spec'], dict)
+        self.assertIn('containers', model['spec'])
+        self.assertIsInstance(model['spec']['containers'], list)
+        self.assertIn('dnsPolicy', model['spec'])
+        self.assertIsInstance(model['spec']['dnsPolicy'], str)
+        self.assertIn('restartPolicy', model['spec'])
+        self.assertIsInstance(model['spec']['restartPolicy'], str)
+        self.assertIn('volumes', model['spec'])
+        self.assertIsInstance(model['spec']['volumes'], list)
+
 
 
 
