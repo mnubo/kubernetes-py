abb589bdb84fa586ec8a5447d8e043dd04d11908
Francis Roch <francis@manafont.net>
2016-07-20 17:02:54 -0400

    K8sSecret: add unit tests for init, structure and methods

--- kubernetes/K8sSecret.py
+++ kubernetes/K8sSecret.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
 from kubernetes.K8sObject import K8sObject
 from kubernetes.models.v1.Secret import Secret
 
@@ -8,48 +16,48 @@ class K8sSecret(K8sObject):
         K8sObject.__init__(self, config=config, obj_type='Secret', name=name)
         self.model = Secret(name=name, namespace=self.config.namespace)
 
-    def add_annotation(self, k, v):
-        assert isinstance(k, str)
-        assert isinstance(v, str)
+    # ------------------------------------------------------------------------------------- add
+
+    def add_annotation(self, k=None, v=None):
         self.model.add_annotation(k=k, v=v)
         return self
 
-    def add_label(self, k, v):
-        assert isinstance(k, str)
-        assert isinstance(v, str)
+    def add_label(self, k=None, v=None):
         self.model.add_label(k=k, v=v)
         return self
+
+    # ------------------------------------------------------------------------------------- get
 
     def get(self):
         self.model = Secret(model=self.get_model())
         return self
 
-    def set_data(self, data_key, data_value):
-        assert isinstance(data_value, str)
-        assert isinstance(data_value, str)
-        self.model.set_data(data_key=data_key, data_value=data_value)
+    # ------------------------------------------------------------------------------------- set
+
+    def set_data(self, k=None, v=None):
+        self.model.set_data(k=k, v=v)
         return self
 
-    def set_type(self, secret_type):
-        assert isinstance(secret_type, str)
+    def set_type(self, secret_type=None):
         self.model.set_type(secret_type=secret_type)
         return self
 
-    def set_dockercfg_secret(self, data):
-        assert isinstance(data, str)
+    def set_dockercfg_secret(self, data=None):
         self.model.set_dockercfg_secret(data=data)
         return self
 
-    def set_dockercfg_json_secret(self, data):
-        assert isinstance(data, str)
+    def set_dockercfg_json_secret(self, data=None):
         self.model.set_dockercfg_json_secret(data=data)
         return self
 
-    def set_service_account_token(self, account_name, account_uid, token,
+    def set_service_account_token(self, account_name=None, account_uid=None, token=None,
                                   kubecfg_data=None, cacert=None):
-        assert isinstance(account_name, str)
-        assert isinstance(account_uid, str)
-        assert isinstance(token, str)
-        self.model.set_service_account_token(account_name=account_name, account_uid=account_uid,
-                                             token=token, kubecfg_data=kubecfg_data, cacert=cacert)
+
+        self.model.set_service_account_token(
+            account_name=account_name,
+            account_uid=account_uid,
+            token=token,
+            kubecfg_data=kubecfg_data,
+            cacert=cacert
+        )
         return self
