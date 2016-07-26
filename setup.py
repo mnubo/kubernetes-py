c95a5caf52de539c21255305f3bc82b5b8ef3d66
Francis Roch <francis@manafont.net>
2016-07-26 17:14:37 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -12,31 +12,32 @@ def version():
     with open(os.path.abspath(__file__).replace('setup.py', 'version.meta'), 'r') as v:
         return v.read()
 
-setup(name='kubernetes-py',
-      version=version(),
-      description='A python module for Kubernetes.',
-      author='mnubo inc.',
-      author_email='scoutu@mnubo.com',
-      url='https://github.com/mnubo/kubernetes-py',
-      download_url='https://github.com/mnubo/kubernetes-py/tarball/' + version(),
-      keywords=['kubernetes', 'k8s'],
-      packages=[
-          'kubernetes',
-          'kubernetes.exceptions',
-          'kubernetes.models',
-          'kubernetes.models.v1',
-          'kubernetes.utils'
-      ],
-      install_requires=[
-          'importlib>=1.0.3',
-          'uuid>=1.30'
-      ],
-      scripts=[],
-      test_suite='nose.collector',
-      tests_require=[
-          'nose', 
-          'nose-cover3'
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='kubernetes-py',
+    version=version(),
+    description='A python module for Kubernetes.',
+    author='mnubo inc.',
+    author_email='scoutu@mnubo.com',
+    url='https://github.com/mnubo/kubernetes-py',
+    download_url='https://github.com/mnubo/kubernetes-py/tarball/' + version(),
+    keywords=['kubernetes', 'k8s'],
+    packages=[
+        'kubernetes',
+        'kubernetes.models',
+        'kubernetes.models.v1',
+        'kubernetes.utils'
+    ],
+    install_requires=[
+        'importlib>=1.0.3',
+        'uuid>=1.30'
+    ],
+    scripts=[],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'nose-cover3'
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
 
