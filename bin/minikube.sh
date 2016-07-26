9666d032586e8d34e6092a7daf5cfa970dbee2a7
Francis Roch <francis@manafont.net>
2016-07-26 18:28:22 -0400

    update minikube to 0.7.0

--- bin/minikube.sh
+++ bin/minikube.sh
@@ -3,10 +3,10 @@
 OS="`uname`"
 case $OS in
   'Linux')
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.6.0/minikube-linux-amd64
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.7.0/minikube-linux-amd64
     ;;
   'Darwin')
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.6.0/minikube-darwin-amd64
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.7.0/minikube-darwin-amd64
     ;;
 esac
 
