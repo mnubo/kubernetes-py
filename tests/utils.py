1afd2d8a41fbb2000402ca3b07b8cfc076b3ec08
Francis Roch <francis@manafont.net>
2016-08-19 14:33:12 -0400

    tests/utils: adding create/cleanup methods for Services

--- tests/utils.py
+++ tests/utils.py
@@ -8,7 +8,15 @@
 
 import os
 import socket
-from kubernetes import K8sConfig, K8sObject, K8sContainer, K8sPod, K8sReplicationController, K8sSecret
+from kubernetes import (
+    K8sConfig,
+    K8sObject,
+    K8sContainer,
+    K8sPod,
+    K8sReplicationController,
+    K8sSecret,
+    K8sService
+)
 from kubernetes.K8sExceptions import NotFoundException
 
 kubeconfig_fallback = '{0}/.kube/config'.format(os.path.abspath(os.path.dirname(os.path.realpath(__file__))))
@@ -85,12 +93,23 @@ def create_secret(config=None, name=None):
     return obj
 
 
+def create_service(config=None, name=None):
+    if config is None:
+        config = create_config()
+    obj = K8sService(
+        config=config,
+        name=name
+    )
+    return obj
+
+
 def cleanup_objects():
     config = K8sConfig(kubeconfig=kubeconfig_fallback)
     if is_reachable(config.api_host):
         cleanup_rcs()
         cleanup_pods()
         cleanup_secrets()
+        cleanup_services()
 
 
 def cleanup_pods():
@@ -134,3 +153,36 @@ def cleanup_secrets():
                 except NotFoundException:
                     continue
             secrets = ref.list()
+
+
+def cleanup_services():
+    ref = create_service(name="throwaway")
+    if is_reachable(ref.config.api_host):
+        services = ref.list()
+        while len(services) > 1:
+            for service in services:
+                try:
+                    obj = K8sService(config=ref.config, name=service['metadata']['name']).get()
+                    if not _is_api_server(service):
+                        obj.delete()
+                except NotFoundException:
+                    continue
+            services = ref.list()
+
+
+def _is_api_server(service=None):
+    if not isinstance(service, dict):
+        return False
+    if 'metadata' not in service:
+        return False
+    if 'labels' not in service['metadata']:
+        return False
+    if 'component' not in service['metadata']['labels']:
+        return False
+    if 'provider' not in service['metadata']['labels']:
+        return False
+    if 'apiserver' != service['metadata']['labels']['component']:
+        return False
+    if 'kubernetes' != service['metadata']['labels']['provider']:
+        return False
+    return True
