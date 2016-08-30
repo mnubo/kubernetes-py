84598f6561a085dae1bb4704a8335ae4f6f5d844
Francis Roch <francis@manafont.net>
2016-08-30 11:10:59 -0400

    K8sReplicaSet: add placeholders; should not be used directly, use K8sDeployments instead.

---
+++ tests/test_k8s_replica_set.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
+import unittest
+import uuid
+import utils
+from kubernetes.K8sExceptions import *
+
+
+class K8sReplicaSetTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        utils.cleanup_objects()
+
+    def test_delete_nonexistent(self):
+        name = "yorc-{0}".format(unicode(uuid.uuid4()))
+        dep = utils.create_deployment(name=name)
+        if utils.is_reachable(dep.config.api_host):
+            try:
+                dep.delete()
+                self.fail("Should not fail.")
+            except Exception as err:
+                self.assertIsInstance(err, NotFoundException)
+
+    def test_delete(self):
+        name = "yoname"
+        rs = utils.create_rs(name=name)
+        config = utils.create_config()
+        if utils.is_reachable(config.api_host):
+            utils.cleanup_rs()
+            result = rs.list()
+            self.assertIsInstance(result, list)
+            self.assertEqual(0, len(result))
