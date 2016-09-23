e4148c4913ccbf3d485c4249a470705f33340e0c
Francis Roch <francis@manafont.net>
2016-09-23 14:11:19 -0400

    models/v1: update exposed modules

--- kubernetes/models/v1/__init__.py
+++ kubernetes/models/v1/__init__.py
@@ -1,25 +1,39 @@
 from BaseModel import BaseModel
+from BaseUrls import BaseUrls
 from Container import Container
 from ContainerStatus import ContainerStatus
 from DeleteOptions import DeleteOptions
 from Deployment import Deployment
 from ObjectMeta import ObjectMeta
+from PersistentVolumeSpec import PersistentVolumeSpec
 from Pod import Pod
 from PodBasedModel import PodBasedModel
 from PodSpec import PodSpec
 from PodStatus import PodStatus
 from Probe import Probe
+from ReplicaSet import ReplicaSet
 from ReplicationController import ReplicationController
 from Secret import Secret
 from Service import Service
+from Volume import Volume
 
 __all__ = [
+    'BaseModel',
+    'BaseUrls',
     'Container',
+    'ContainerStatus',
     'DeleteOptions',
     'Deployment',
+    'ObjectMeta',
+    'PersistentVolumeSpec',
     'Pod',
+    'PodBasedModel',
     'PodSpec',
+    'PodStatus',
+    'Probe',
+    'ReplicaSet',
     'ReplicationController',
     'Secret',
-    'Service'
+    'Service',
+    'Volume'
 ]
