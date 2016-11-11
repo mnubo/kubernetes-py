6326dd394b88caa70135bb2d8a25156909d79157
Francis Roch <francis@manafont.net>
2016-11-11 15:24:20 -0500

    update v1.LoadBalancerStatus

--- kubernetes/models/v1/LoadBalancerStatus.py
+++ kubernetes/models/v1/LoadBalancerStatus.py
@@ -17,9 +17,20 @@ class LoadBalancerStatus(object):
     http://kubernetes.io/docs/api-reference/v1/definitions/#_v1_loadbalancerstatus
     """
 
-    def __init__(self):
+    def __init__(self, model=None):
         super(LoadBalancerStatus, self).__init__()
         self._ingress = None
+
+        if model is not None:
+            self._build_with_model(model)
+
+    def _build_with_model(self, model=None):
+        if 'ingress' in model:
+            statuses = []
+            for i in model['ingress']:
+                status = LoadBalancerIngress(model=i)
+                statuses.append(status)
+            self.ingress = statuses
 
     # ------------------------------------------------------------------------------------- ingress
 
@@ -35,8 +46,8 @@ class LoadBalancerStatus(object):
 
     # ------------------------------------------------------------------------------------- serialize
 
-    def json(self):
+    def serialize(self):
         data = {}
         if self.ingress is not None:
-            data['ingress'] = [x.json() for x in self.ingress]
+            data['ingress'] = [x.serialize() for x in self.ingress]
         return data
