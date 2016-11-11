024dc5e2e82e0785ec2ed59604e29a5d7a627d32
Francis Roch <francis@manafont.net>
2016-11-11 15:28:25 -0500

    update K8sObject

--- kubernetes/K8sObject.py
+++ kubernetes/K8sObject.py
@@ -10,7 +10,12 @@ from kubernetes.K8sConfig import K8sConfig
 from kubernetes.K8sExceptions import *
 from kubernetes.models.v1.BaseUrls import BaseUrls
 from kubernetes.models.v1.DeleteOptions import DeleteOptions
-from kubernetes.utils import HttpRequest, is_valid_dict
+from kubernetes.models.v1.BaseModel import BaseModel
+from kubernetes.models.v1.Pod import Pod
+from kubernetes.models.v1.ReplicationController import ReplicationController
+from kubernetes.models.v1.Secret import Secret
+from kubernetes.models.v1.Service import Service
+from kubernetes.utils import HttpRequest, is_valid_dict, str_to_class
 
 VALID_K8s_OBJS = [
     'Deployment',
@@ -25,8 +30,7 @@ VALID_K8s_OBJS = [
 
 
 class K8sObject(object):
-
-    def __init__(self, config=None, name=None, obj_type=None):
+    def __init__(self, config=None, obj_type=None, name=None):
         super(K8sObject, self).__init__()
 
         if config is not None and not isinstance(config, K8sConfig):
@@ -35,19 +39,12 @@ class K8sObject(object):
             config = K8sConfig()
         self.config = config
 
-        if name is None:
-            raise SyntaxError('K8sObject: name: [ {0} ] cannot be None.'.format(name))
-        if not isinstance(name, str):
-            raise SyntaxError('K8sObject: name: [ {0} ] must be a string.'.format(name.__class__.__name__))
-
-        if obj_type is None or not isinstance(obj_type, str):
-            raise SyntaxError('K8sObject: obj_type: [ {0} ] must be a string.'.format(obj_type.__class__.__name__))
-
         if obj_type not in VALID_K8s_OBJS:
             valid = ", ".join(VALID_K8s_OBJS)
             raise SyntaxError('K8sObject: obj_type: [ {0} ] must be in: [ {1} ]'.format(obj_type, valid))
-
         self.obj_type = obj_type
+
+        self.model = str_to_class(obj_type)
         self.name = name
 
         try:
@@ -57,7 +54,7 @@ class K8sObject(object):
             raise Exception('Could not set BaseUrl for type: [ {0} ]'.format(obj_type))
 
     def __str__(self):
-        return "{0}".format(self.model.get())
+        return "{}".format(self.model.serialize())
 
     def __eq__(self, other):
         # see https://github.com/kubernetes/kubernetes/blob/release-1.3/docs/design/identifiers.md
@@ -65,15 +62,15 @@ class K8sObject(object):
             return self.config.namespace == other.config.namespace and self.name == other.name
         return NotImplemented
 
-    # ------------------------------------------------------------------------------------- set name
+    # ------------------------------------------------------------------------------------- name
 
-    def set_name(self, name):
-        self.name = name
-        if self.model is not None:
-            meth = getattr(self.model, "set_name", None)
-            if callable(meth):
-                meth(name=name)
-        return self
+    @property
+    def name(self):
+        return self.model.metadata.name
+
+    @name.setter
+    def name(self, name=None):
+        self.model.metadata.name = name
 
     # ------------------------------------------------------------------------------------- serialize
 
@@ -85,12 +82,12 @@ class K8sObject(object):
     def as_json(self):
         if self.model is None:
             return ""
-        return self.model.as_json()
+        return ""
 
     def as_yaml(self):
         if self.model is None:
             return ""
-        return self.model.as_yaml()
+        return ""
 
     # ------------------------------------------------------------------------------------- remote API calls
 
@@ -168,6 +165,10 @@ class K8sObject(object):
         if self.name is None:
             raise SyntaxError('K8sObject: name: [ {0} ] must be set to CREATE the object.'.format(self.name))
 
+        # HTTP 500 : resourceVersion may not be set on objects to be created
+        if self.model.metadata.resource_version is not None:
+            self.model.metadata.resource_version = None
+
         url = '{base}'.format(base=self.base_url)
         state = self.request(method='POST', url=url, data=self.serialize())
 
@@ -188,6 +189,10 @@ class K8sObject(object):
     def update(self):
         if self.name is None:
             raise SyntaxError('K8sObject: name: [ {0} ] must be set to UPDATE the object.'.format(self.name))
+
+        # HTTP 409: Cannot apply update if UID in precondition and updated object.meta
+        if self.model.metadata.uid is not None:
+            self.model.metadata.uid = None
 
         url = '{base}/{name}'.format(base=self.base_url, name=self.name)
         state = self.request(method='PUT', url=url, data=self.serialize())
