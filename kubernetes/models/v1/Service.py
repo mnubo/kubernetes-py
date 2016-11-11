7301b0e60f391b37bd8e7221c74da7539a7324c1
Francis Roch <francis@manafont.net>
2016-11-11 15:26:40 -0500

    update v1.Service

--- kubernetes/models/v1/Service.py
+++ kubernetes/models/v1/Service.py
@@ -6,11 +6,11 @@
 # file 'LICENSE.md', which is part of this source code package.
 #
 
-from kubernetes.models.v1 import (
-    ObjectMeta,
-    ServiceSpec,
-    ServiceStatus
-)
+from kubernetes.models.v1.ObjectMeta import ObjectMeta
+from kubernetes.models.v1.ServiceSpec import ServiceSpec
+from kubernetes.models.v1.ServiceStatus import ServiceStatus
+from kubernetes.models.v1.ServicePort import ServicePort
+from kubernetes.utils import is_valid_dict, is_valid_string
 
 
 class Service(object):
@@ -18,15 +18,90 @@ class Service(object):
     http://kubernetes.io/docs/api-reference/v1/definitions/#_v1_service
     """
 
-    def __init__(self):
+    def __init__(self, model=None):
         super(Service, self).__init__()
-
-        self._metadata = None
-        self._spec = None
-        self._status = None
-
         self.kind = 'Service'
         self.api_version = 'v1'
+
+        self._metadata = ObjectMeta()
+        self._spec = ServiceSpec()
+        self._status = ServiceStatus()
+
+        if model is not None:
+            self._build_with_model(model)
+
+    def _build_with_model(self, model=None):
+        if 'kind' in model:
+            self.kind = model['kind']
+        if 'apiVersion' in model:
+            self.api_version = model['apiVersion']
+        if 'metadata' in model:
+            self.metadata = ObjectMeta(model=model['metadata'])
+        if 'spec' in model:
+            self.spec = ServiceSpec(model=model['spec'])
+        if 'status' in model:
+            self.status = ServiceStatus(model=model['status'])
+
+    # ------------------------------------------------------------------------------------- add
+
+    def add_annotation(self, k=None, v=None):
+        anns = self.metadata.annotations
+        if anns is None:
+            anns = {}
+        anns.update({k: v})
+        self.metadata.annotations = anns
+        return self
+
+    def add_label(self, k=None, v=None):
+        labels = self.metadata.labels
+        if labels is None:
+            labels = {}
+        labels.update({k: v})
+        self.metadata.labels = labels
+        return self
+
+    def add_port(self, name=None, port=None, target_port=None, protocol=None, node_port=None):
+        if not is_valid_string(name):
+            raise SyntaxError('Service.add_port() name: [ {} ] is invalid.'.format(name))
+        ports = self.spec.ports
+        filtered = filter(lambda x: x.name == name, ports)
+        inverse = filter(lambda x: x.name != name, ports)
+        if not filtered:
+            sp = ServicePort(name=name)
+        else:
+            sp = filtered[0]
+        if port:
+            sp.port = port
+        if target_port:
+            sp.target_port = target_port
+        if protocol:
+            sp.protocol = protocol
+        if node_port:
+            sp.node_port = node_port
+        inverse.append(sp)
+        self.spec.ports = inverse
+
+    def add_selector(self, selector=None):
+        if not is_valid_dict(selector):
+            raise SyntaxError('Service.add_selector() selector: [ {} ] is invalid.'.format(selector))
+        s = self.spec.selector
+        if s is None:
+            s = {}
+        s.update(selector)
+        self.spec.selector = s
+
+    # ------------------------------------------------------------------------------------- name
+
+    @property
+    def name(self):
+        return self.metadata.name
+
+    @name.setter
+    def name(self, name=None):
+        if not is_valid_string(name):
+            raise SyntaxError('Service: name: [ {0} ] is invalid.'.format(name))
+        self.metadata.name = name
+        self.add_label('name', name)
 
     # ------------------------------------------------------------------------------------- metadata
 
@@ -37,7 +112,7 @@ class Service(object):
     @metadata.setter
     def metadata(self, metadata=None):
         if not isinstance(metadata, ObjectMeta):
-            raise SyntaxError('Pod: metadata: [ {0} ] is invalid.'.format(metadata))
+            raise SyntaxError('Service: metadata: [ {0} ] is invalid.'.format(metadata))
         self._metadata = metadata
 
     # ------------------------------------------------------------------------------------- spec
@@ -49,7 +124,7 @@ class Service(object):
     @spec.setter
     def spec(self, spec=None):
         if not isinstance(spec, ServiceSpec):
-            raise SyntaxError('Pod: spec: [ {0} ] is invalid.'.format(spec))
+            raise SyntaxError('Service: spec: [ {0} ] is invalid.'.format(spec))
         self._spec = spec
 
     # ------------------------------------------------------------------------------------- status
@@ -61,21 +136,21 @@ class Service(object):
     @status.setter
     def status(self, status=None):
         if not isinstance(status, ServiceStatus):
-            raise SyntaxError('Pod: status: [ {0} ] is invalid.'.format(status))
+            raise SyntaxError('Service: status: [ {0} ] is invalid.'.format(status))
         self._status = status
 
     # ------------------------------------------------------------------------------------- serialize
 
-    def json(self):
+    def serialize(self):
         data = {}
         if self.kind:
             data['kind'] = self.kind
         if self.api_version:
-            data['api_version'] = self.api_version
+            data['apiVersion'] = self.api_version
         if self.metadata is not None:
-            data['metadata'] = self.metadata.json()
+            data['metadata'] = self.metadata.serialize()
         if self.spec is not None:
-            data['spec'] = self.spec.json()
+            data['spec'] = self.spec.serialize()
         if self.status is not None:
-            data['status'] = self.status.json()
+            data['status'] = self.status.serialize()
         return data
