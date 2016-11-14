24a245fc3309a79e06e435b18582f70dfadab019
Francis Roch <francis@manafont.net>
2016-11-14 17:32:25 -0500

    update K8sReplicaSet

--- kubernetes/K8sReplicaSet.py
+++ kubernetes/K8sReplicaSet.py
@@ -6,35 +6,17 @@
 # file 'LICENSE.md', which is part of this source code package.
 #
 
-from kubernetes.K8sPodBasedObject import K8sPodBasedObject
-from kubernetes.models.v1.ReplicaSet import ReplicaSet
+from kubernetes.K8sObject import K8sObject
+from kubernetes.models.v1beta1.ReplicaSet import ReplicaSet
 
 
-class K8sReplicaSet(K8sPodBasedObject):
+class K8sReplicaSet(K8sObject):
     """
-    This module uses K8sReplicaSet for HTTP DELETE operations.
-
-    From http://kubernetes.io/docs/user-guide/replicasets/:
-
-    While Replica Sets can be used independently, today it’s mainly used by Deployments as
-    a mechanism to orchestrate pod creation, deletion and updates. When you use Deployments
-    you don’t have to worry about managing the Replica Sets that they create.
-    Deployments own and manage their Replica Sets.
-
-    A Replica Set ensures that a specified number of pod “replicas” are running at any given time.
-    However, a Deployment is a higher-level concept that manages Replica Sets and provides declarative
-    updates to pods along with a lot of other useful features. Therefore, we recommend using Deployments
-    instead of directly using Replica Sets, unless you require custom update orchestration or don’t
-    require updates at all.
-
-    This actually means that you may never need to manipulate Replica Set objects: use directly a
-    Deployment and define your application in the spec section.
-
+    http://kubernetes.io/docs/api-reference/extensions/v1beta1/definitions/#_v1beta1_replicaset
     """
 
     def __init__(self, config=None, name=None):
         super(K8sReplicaSet, self).__init__(config=config, obj_type='ReplicaSet', name=name)
-        self.model = ReplicaSet(name=name, namespace=self.config.namespace)
 
     # -------------------------------------------------------------------------------------  get
 
