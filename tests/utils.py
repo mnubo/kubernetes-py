cf290bde3b11138abc0cdf830f049f079fda76e4
Francis Roch <francis@manafont.net>
2016-11-15 15:39:37 -0500

    tests.utils: fixes create_volume()

--- tests/utils.py
+++ tests/utils.py
@@ -165,15 +165,10 @@ def create_service(config=None, name=None):
     return obj
 
 
-def create_volume(config=None, name=None, type=None, mount_path=None, read_only=False):
-    if config is None:
-        config = create_config()
+def create_volume(name=None, type=None):
     obj = K8sVolume(
-        config=config,
         name=name,
         type=type,
-        mount_path=mount_path,
-        read_only=read_only
     )
     return obj
 
