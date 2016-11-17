6f6b6cd10abc3212ea54495bd614d0ccd333bb50
Francis Roch <francis@manafont.net>
2016-11-17 13:48:22 -0500

    tests.utils: add helpers for volume mounts, persistent volumes and pv claims

--- tests/utils.py
+++ tests/utils.py
@@ -15,12 +15,14 @@ from kubernetes.K8sDeployment import K8sDeployment
 from kubernetes.K8sExceptions import NotFoundException
 from kubernetes.K8sObject import K8sObject
 from kubernetes.K8sPersistentVolume import K8sPersistentVolume
+from kubernetes.K8sPersistentVolumeClaim import K8sPersistentVolumeClaim
 from kubernetes.K8sPod import K8sPod
 from kubernetes.K8sReplicaSet import K8sReplicaSet
 from kubernetes.K8sReplicationController import K8sReplicationController
 from kubernetes.K8sSecret import K8sSecret
 from kubernetes.K8sService import K8sService
 from kubernetes.K8sVolume import K8sVolume
+from kubernetes.K8sVolumeMount import K8sVolumeMount
 
 kubeconfig_fallback = '{0}/.kube/config'.format(os.path.abspath(os.path.dirname(os.path.realpath(__file__))))
 
@@ -174,13 +176,31 @@ def create_volume(name=None, type=None):
     return obj
 
 
-def create_persistent_volume(config=None, name=None, type=None):
+def create_volume_mount(name=None, mount_path=None):
+    obj = K8sVolumeMount(
+        name=name,
+        mount_path=mount_path,
+    )
+    return obj
+
+
+def create_pv(config=None, name=None, type=None):
     if config is None:
         config = create_config()
     obj = K8sPersistentVolume(
         config=config,
         name=name,
         type=type,
+    )
+    return obj
+
+
+def create_pvc(config=None, name=None):
+    if config is None:
+        config = create_config()
+    obj = K8sPersistentVolumeClaim(
+        config=config,
+        name=name
     )
     return obj
 
@@ -285,8 +305,8 @@ def cleanup_deployments():
             deps = ref.list()
 
 
-def cleanup_persistent_volumes():
-    ref = create_persistent_volume(name="throwaway", type="hostPath")
+def cleanup_pv():
+    ref = create_pv(name="throwaway", type="hostPath")
     if is_reachable(ref.config.api_host):
         vols = ref.list()
         while len(vols) > 0:
@@ -297,3 +317,17 @@ def cleanup_persistent_volumes():
                 except NotFoundException:
                     continue
             vols = ref.list()
+
+
+def cleanup_pvc():
+    ref = create_pvc(name="throwaway")
+    if is_reachable(ref.config.api_host):
+        claims = ref.list()
+        while len(claims) > 0:
+            for c in claims:
+                try:
+                    claim = K8sPersistentVolumeClaim(config=ref.config, name=c['metadata']['name']).get()
+                    claim.delete()
+                except NotFoundException:
+                    continue
+            claims = ref.list()
