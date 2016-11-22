6432f3ff58011f5d1276ed9e3fe490c1b80c6803
Francis Roch <francis@manafont.net>
2016-11-21 19:22:48 -0500

    update v1.ContainerPort

--- kubernetes/models/v1/ContainerPort.py
+++ kubernetes/models/v1/ContainerPort.py
@@ -6,7 +6,7 @@
 # file 'LICENSE.md', which is part of this source code package.
 #
 
-from kubernetes.utils import filter_model
+from kubernetes.utils import filter_model, is_valid_string
 
 
 class ContainerPort(object):
@@ -19,19 +19,27 @@ class ContainerPort(object):
     def __init__(self, model=None):
         super(ContainerPort, self).__init__()
 
-        self._container_port = None
+        self._name = None
         self._host_port = None
+        self._container_port = None
         self._protocol = 'TCP'
-
-        self.name = None
-        self.host_ip = None
+        self._host_ip = None
 
         if model is not None:
             m = filter_model(model)
             self._build_with_model(m)
 
     def _build_with_model(self, model=None):
-        pass
+        if 'name' in model:
+            self.name = model['name']
+        if 'hostPort' in model:
+            self.host_port = model['hostPort']
+        if 'containerPort' in model:
+            self.container_port = model['containerPort']
+        if 'protocol' in model:
+            self.protocol = model['protocol']
+        if 'hostIP' in model:
+            self.host_ip = model['hostIP']
 
     @staticmethod
     def _is_valid_port(port=None):
@@ -41,17 +49,17 @@ class ContainerPort(object):
             return False
         return True
 
-    # ------------------------------------------------------------------------------------- container port
+    # ------------------------------------------------------------------------------------- name
 
     @property
-    def container_port(self):
-        return self._container_port
+    def name(self):
+        return self._name
 
-    @container_port.setter
-    def container_port(self, port=None):
-        if not ContainerPort._is_valid_port(port):
-            raise SyntaxError('ContainerPort: container_port: [ {0} ] is invalid.'.format(port))
-        self._container_port = port
+    @name.setter
+    def name(self, name=None):
+        if not is_valid_string(name):
+            raise SyntaxError('ContainerPort: name: [ {0} ] is invalid.'.format(name))
+        self._name = name
 
     # ------------------------------------------------------------------------------------- host port
 
@@ -65,6 +73,18 @@ class ContainerPort(object):
             raise SyntaxError('ContainerPort: host_port: [ {0} ] is invalid.'.format(port))
         self._host_port = port
 
+    # ------------------------------------------------------------------------------------- container port
+
+    @property
+    def container_port(self):
+        return self._container_port
+
+    @container_port.setter
+    def container_port(self, port=None):
+        if not ContainerPort._is_valid_port(port):
+            raise SyntaxError('ContainerPort: container_port: [ {0} ] is invalid.'.format(port))
+        self._container_port = port
+
     # ------------------------------------------------------------------------------------- protocol
 
     @property
@@ -76,6 +96,18 @@ class ContainerPort(object):
         if protocol.upper() not in ContainerPort.VALID_PROTOCOLS:
             raise SyntaxError('ContainerPort: protocol: [ {0} ] is invalid.'.format(protocol))
         self._protocol = protocol.upper()
+
+    # ------------------------------------------------------------------------------------- hostIP
+
+    @property
+    def host_ip(self):
+        return self._host_ip
+
+    @host_ip.setter
+    def host_ip(self, ip=None):
+        if not is_valid_string(ip):
+            raise SyntaxError('ContainerPort: host_ip: [ {0} ] is invalid.'.format(ip))
+        self._host_ip = ip
 
     # ------------------------------------------------------------------------------------- serialize
 
