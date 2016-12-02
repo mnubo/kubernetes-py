b618071e4f90ca26b97152214257475402a41d79
Francis Roch <francis@manafont.net>
2016-12-02 15:12:17 -0500

    K8sObject: add support for ‘orphan’, ie. cascade on delete

--- kubernetes/K8sObject.py
+++ kubernetes/K8sObject.py
@@ -272,12 +272,16 @@ class K8sObject(object):
 
         return self
 
-    def delete(self):
+    def delete(self, orphan=False):
         if self.name is None:
             raise SyntaxError('K8sObject: name: [ {0} ] must be set to DELETE the object.'.format(self.name))
 
         url = '{base}/{name}'.format(base=self.base_url, name=self.name)
-        state = self.request(method='DELETE', url=url, data=DeleteOptions().serialize())
+
+        delete_opts = DeleteOptions()
+        delete_opts.orphan_dependents = orphan
+
+        state = self.request(method='DELETE', url=url, data=delete_opts.serialize())
 
         if not state.get('success'):
             status = state.get('status', '')
