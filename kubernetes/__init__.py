763b2108d95af4a293527499620e26312b51d5d5
Francis Roch <francis@manafont.net>
2016-12-05 14:50:12 -0500

    rework kubernetes.__init__.py

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -9,22 +9,16 @@
 from K8sConfig import K8sConfig
 from K8sContainer import K8sContainer
 from K8sDeployment import K8sDeployment
+from K8sExceptions import *
 from K8sJob import K8sJob
 from K8sObject import K8sObject
+from K8sPersistentVolume import K8sPersistentVolume
+from K8sPersistentVolumeClaim import K8sPersistentVolumeClaim
 from K8sPod import K8sPod
+# from K8sReplicaSet import K8sReplicaSet  # should not be used directly
 from K8sReplicationController import K8sReplicationController
 from K8sSecret import K8sSecret
 from K8sService import K8sService
 from K8sVolume import K8sVolume
+from K8sVolumeMount import K8sVolumeMount
 
-__all__ = [
-    'K8sConfig',
-    'K8sContainer',
-    'K8sDeployment',
-    'K8sObject'
-    'K8sPod',
-    'K8sReplicationController',
-    'K8sSecret',
-    'K8sService',
-    'K8sVolume'
-]
