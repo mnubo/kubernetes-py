d9f10ce323a93a179e1de42dbf2984df84bdf0a5
Francis Roch <francis@manafont.net>
2016-12-15 14:14:52 -0500

    K8sCronJob: adding manipulation of containers

--- kubernetes/K8sCronJob.py
+++ kubernetes/K8sCronJob.py
@@ -7,7 +7,9 @@
 #
 
 from kubernetes.K8sObject import K8sObject
+from kubernetes.K8sContainer import K8sContainer
 from kubernetes.models.v2alpha1.CronJob import CronJob
+from kubernetes.utils import is_valid_list
 
 
 class K8sCronJob(K8sObject):
@@ -35,6 +37,20 @@ class K8sCronJob(K8sObject):
 
     def get(self):
         self.model = CronJob(model=self.get_model())
+        return self
+
+    # -------------------------------------------------------------------------------------  add
+
+    def add_container(self, container=None):
+        if not isinstance(container, K8sContainer):
+            raise SyntaxError(
+                'K8sCronJob.add_container() container: [ {0} ] is invalid.'.format(container))
+        containers = self.model.spec.job_template.spec.template.spec.containers
+        if containers is None:
+            containers = []
+        filtered = filter(lambda x: x.name != container.name, containers)
+        filtered.append(container.model)
+        self.model.spec.job_template.spec.template.spec.containers = filtered
         return self
 
     # -------------------------------------------------------------------------------------  schedule
@@ -96,3 +112,44 @@ class K8sCronJob(K8sObject):
     @completions.setter
     def completions(self, c=None):
         self.model.spec.job_template.spec.completions = c
+
+    # -------------------------------------------------------------------------------------  containers
+
+    @property
+    def containers(self):
+        objs = []
+        for c in self.model.spec.job_template.spec.template.spec.containers:
+            k8scontainer = K8sContainer(name=c.name, image=c.image)
+            k8scontainer.model = c
+            objs.append(k8scontainer)
+        return objs
+
+    @containers.setter
+    def containers(self, containers=None):
+        if not is_valid_list(containers, K8sContainer):
+            raise SyntaxError('K8sCronJob: containers: [ {} ] is invalid.'.format(containers))
+        models = []
+        for obj in containers:
+            models.append(obj.model)
+        self.model.spec.job_template.spec.template.spec.containers = models
+
+    # -------------------------------------------------------------------------------------  container_image
+
+    @property
+    def container_image(self):
+        data = {}
+        for c in self.containers:
+            data[c.name] = c.image
+        return data
+
+    @container_image.setter
+    def container_image(self, tup=None):
+        if not isinstance(tup, tuple):
+            raise SyntaxError('K8sCronJob.container_image() must be a tuple of the form (name, image)')
+        name, image = tup
+        found = filter(lambda x: x.name == name, self.containers)
+        if found:
+            new = filter(lambda x: x.name != name, self.containers)
+            found[0].image = image
+            new.append(found[0])
+            self.containers = new
