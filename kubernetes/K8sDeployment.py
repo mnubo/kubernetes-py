00f20e26674447c4db11a2116b5c4876b3110cba
Francis Roch <francis@manafont.net>
2016-12-20 12:24:00 -0500

    K8sDeployment: tweak timeout message

--- kubernetes/K8sDeployment.py
+++ kubernetes/K8sDeployment.py
@@ -79,7 +79,7 @@ class K8sDeployment(K8sObject):
         elapsed_time = time.time() - start_time
         if elapsed_time >= self.SCALE_WAIT_TIMEOUT_SECONDS:  # timeout
             raise TimedOutException(
-                "Timed out scaling Deployment: [ {} ] to replica count: [ {1} ]".format(
+                "Timed out scaling Deployment: [ {} ] to replica count: [ {} ]".format(
                     self.name,
                     self.desired_replicas))
 
