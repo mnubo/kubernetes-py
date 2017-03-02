f15c1124af57dc339a0b18531c1dc58f9595bfb8
Francis Roch <francis@manafont.net>
2017-03-02 14:32:23 -0500

    adding K8sStatefulSet and related models

---
+++ kubernetes/K8sStatefulSet.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
+from kubernetes.K8sObject import K8sObject
+from kubernetes.models.v1beta1.StatefulSet import StatefulSet
+
+
+class K8sStatefulSet(K8sObject):
+    """
+    https://kubernetes.io/docs/api-reference/apps/v1beta1/definitions/#_v1beta1_statefulset
+    """
+
+    def __init__(self, config=None, name=None):
+        super(K8sStatefulSet, self).__init__(
+            config=config,
+            name=name
+        )
+
+    # -------------------------------------------------------------------------------------  override
+
+    def get(self):
+        self.model = StatefulSet(self.get_model())
+        return self
+
+    def create(self):
+        super(K8sStatefulSet, self).create()
+        self.get()
+        return self
+
+    def update(self):
+        super(K8sStatefulSet, self).update()
+        self.get()
+        return self
