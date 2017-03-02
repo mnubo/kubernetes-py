dd26529b67ee266e3ab30d3e0b51f10b87ed62b3
Francis Roch <francis@manafont.net>
2017-03-02 15:42:50 -0500

    tests: adding placeholders for nginx and postgres under statefulsets

---
+++ tests/wip_stateful_nginx.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+#
+
+import unittest
+
+
+class StatefulSetNginxTests(unittest.TestCase):
+    """
+    https://github.com/kubernetes/kubernetes/blob/master/hack/testdata/nginx-statefulset.yaml
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
