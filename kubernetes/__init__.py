2ff698e8d688a5fd61bcbf42c0aae6816640d4fe
Francis Roch <francis@manafont.net>
2017-03-13 11:57:00 -0400

    kubernetes: fixes exposed imports from main module

--- kubernetes/__init__.py
+++ kubernetes/__init__.py
@@ -9,6 +9,8 @@
 from K8sComponentStatus import K8sComponentStatus
 from K8sConfig import K8sConfig
 from K8sContainer import K8sContainer
+from K8sCronJob import K8sCronJob
+from K8sDaemonSet import K8sDaemonSet
 from K8sDeployment import K8sDeployment
 from K8sExceptions import *
 from K8sJob import K8sJob
@@ -17,11 +19,14 @@ from K8sNode import K8sNode
 from K8sObject import K8sObject
 from K8sPersistentVolume import K8sPersistentVolume
 from K8sPersistentVolumeClaim import K8sPersistentVolumeClaim
+from K8sPetSet import K8sPetSet
 from K8sPod import K8sPod
 # from K8sReplicaSet import K8sReplicaSet  # should not be used directly
 from K8sReplicationController import K8sReplicationController
 from K8sSecret import K8sSecret
 from K8sService import K8sService
+from K8sServiceAccount import K8sServiceAccount
+from K8sStatefulSet import K8sStatefulSet
+from K8sStorageClass import K8sStorageClass
 from K8sVolume import K8sVolume
 from K8sVolumeMount import K8sVolumeMount
-
