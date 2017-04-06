db53128f22fbdb102e40f69edf0e89a91bcfadd4
Francis Roch <francis@manafont.net>
2017-04-06 14:00:03 -0400

    adding models.v1beta1 for Horizontal Pod Autoscaler

--- kubernetes/models/v1beta1/HorizontalPodAutoscaler.py
+++ kubernetes/models/v1beta1/HorizontalPodAutoscaler.py
@@ -7,10 +7,69 @@
 #
 
 from kubernetes.models.unversioned import BaseModel
+from kubernetes.models.v1.ObjectMeta import ObjectMeta
+from kubernetes.models.v1beta1.HorizontalPodAutoscalerSpec import HorizontalPodAutoscalerSpec
+from kubernetes.models.v1beta1.HorizontalPodAutoscalerStatus import HorizontalPodAutoscalerStatus
 
 
 class HorizontalPodAutoscaler(BaseModel):
+    """
+    https://kubernetes.io/docs/api-reference/extensions/v1beta1/definitions/#_v1beta1_horizontalpodautoscaler
+    """
 
-    def __init__(self):
+    def __init__(self, model):
         super(HorizontalPodAutoscaler, self).__init__()
 
+        self._kind = 'HorizontalPodAutoscaler'
+        self._api_version = 'extensions/v1beta1'
+        self._spec = HorizontalPodAutoscalerSpec()
+        self._status = HorizontalPodAutoscalerStatus()
+
+        if model is not None:
+            self._build_with_model(model)
+
+    def _build_with_model(self, model=None):
+        if 'kind' in model:
+            self.kind = model['kind']
+        if 'apiVersion' in model:
+            self.api_version = model['apiVersion']
+        if 'metadata' in model:
+            self.metadata = ObjectMeta(model['metadata'])
+        if 'spec' in model:
+            self.spec = HorizontalPodAutoscalerSpec(model['spec'])
+        if 'status' in model:
+            self.status = HorizontalPodAutoscalerStatus(model['status'])
+
+    # ------------------------------------------------------------------------------------- spec
+
+    @property
+    def spec(self):
+        return self._spec
+
+    @spec.setter
+    def spec(self, s=None):
+        if not isinstance(s, HorizontalPodAutoscalerSpec):
+            raise SyntaxError('HorizontalPodAutoscaler: spec: [ {} ] is invalid.'.format(s))
+        self._spec = s
+
+    # ------------------------------------------------------------------------------------- status
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, s=None):
+        if not isinstance(s, HorizontalPodAutoscalerStatus):
+            raise SyntaxError('HorizontalPodAutoscaler: status: [ {} ] is invalid.'.format(s))
+        self._status = s
+
+    # ------------------------------------------------------------------------------------- serialize
+
+    def serialize(self):
+        data = super(HorizontalPodAutoscaler, self).serialize()
+        if self.spec is not None:
+            data['spec'] = self.spec.serialize()
+        if self.status is not None:
+            data['status'] = self.status.serialize()
+        return data
--- kubernetes/models/v1beta1/HorizontalPodAutoscalerSpec.py
+++ kubernetes/models/v1beta1/HorizontalPodAutoscalerSpec.py
@@ -6,9 +6,94 @@
 # file 'LICENSE.md', which is part of this source code package.
 #
 
+from kubernetes.models.v1beta1.CPUTargetUtilization import CPUTargetUtilization
+from kubernetes.models.v1beta1.SubresourceReference import SubresourceReference
+
 
 class HorizontalPodAutoscalerSpec(object):
+    """
+    https://kubernetes.io/docs/api-reference/extensions/v1beta1/definitions/#_v1beta1_horizontalpodautoscalerspec
+    """
 
-    def __init__(self):
+    def __init__(self, model=None):
         super(HorizontalPodAutoscalerSpec, self).__init__()
 
+        self._scale_ref = None
+        self._min_replicas = 1
+        self._max_replicas = 1  # cannot be smaller than min_replicas
+        self._cpu_utilization = None
+
+        if model is not None:
+            self._build_with_model(model)
+
+    def _build_with_model(self, model=None):
+        if 'scaleRef' in model:
+            self.scale_ref = SubresourceReference(model['scaleRef'])
+        if 'minReplicas' in model:
+            self.min_replicas = model['minReplicas']
+        if 'maxReplicas' in model:
+            self.max_replicas = model['maxReplicas']
+        if 'cpuUtilization' in model:
+            self.cpu_utilization = CPUTargetUtilization(model['cpuUtilization'])
+
+    # ------------------------------------------------------------------------------------- scaleRef
+
+    @property
+    def scale_ref(self):
+        return self._scale_ref
+
+    @scale_ref.setter
+    def scale_ref(self, ref=None):
+        if not isinstance(ref, SubresourceReference):
+            raise SyntaxError('HorizontalPodAutoscaler: scale_ref: [ {} ] is invalid.'.format(ref))
+        self._scale_ref = ref
+
+    # ------------------------------------------------------------------------------------- minReplicas
+        
+    @property
+    def min_replicas(self):
+        return self._min_replicas
+
+    @min_replicas.setter
+    def min_replicas(self, r=None):
+        if not isinstance(r, int):
+            raise SyntaxError('HorizontalPodAutoscaler: min_replicas: [ {} ] is invalid.'.format(r))
+        self._min_replicas = r
+
+    # ------------------------------------------------------------------------------------- maxReplicas
+
+    @property
+    def max_replicas(self):
+        return self._max_replicas
+
+    @max_replicas.setter
+    def max_replicas(self, r=None):
+        if not isinstance(r, int):
+            raise SyntaxError('HorizontalPodAutoscaler: max_replicas: [ {} ] is invalid.'.format(r))
+        self._max_replicas = r
+
+    # ------------------------------------------------------------------------------------- cpuUtilization
+
+    @property
+    def cpu_utilization(self):
+        return self._cpu_utilization
+
+    @cpu_utilization.setter
+    def cpu_utilization(self, u=None):
+        if not isinstance(u, CPUTargetUtilization):
+            raise SyntaxError('HorizontalPodAutoscaler: cpu_utilization: [ {} ] is invalid.'.format(u))
+        self._cpu_utilization = u
+
+    # ------------------------------------------------------------------------------------- serialize()
+
+    def serialize(self):
+        data = {}
+        if self.scale_ref is not None:
+            data['scaleRef'] = self.scale_ref.serialize()
+        if self.min_replicas is not None:
+            data['minReplicas'] = self.min_replicas
+        if self.max_replicas is not None:
+            data['maxReplicas'] = self.max_replicas
+        if self.cpu_utilization is not None:
+            data['cpuUtilization'] = self.cpu_utilization.serialize()
+        return data
